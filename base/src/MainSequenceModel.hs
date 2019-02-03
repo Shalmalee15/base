b0aedd6c088db484081d55a2fd61fab71fa9c23e
Elliot Robinson <elliot.robinson@argiopetech.com>
2019-02-03 15:31:17 -0500

    Limbo-ing! See tags/releases for what you'd normally expect in a commit message.

--- base/src/MainSequenceModel.hs
+++ base/src/MainSequenceModel.hs
@@ -4,7 +4,14 @@ import Data.Char (isSpace)
 import Data.Text
 import Data.Attoparsec.Text
 
-data Model = Model { filters :: [Text] }
+data MSModelFormat = Filters [Text]
+                   | Comment Text
+                   deriving (Show, Eq)
 
+parseFilters =
+  let parser = "%f" *> many1 (space *> takeWhile1 (not . isSpace)) <* endOfLine
+  in Filters <$> parser <?> "MS Model filters"
 
-parseFilters = string "%f" *> many1 (space *> takeWhile1 (not . isSpace)) <* endOfLine
+parseComment =
+  let parser = "#" *> takeTill (inClass "\n\r") <* endOfLine
+  in Comment <$> parser <?> "MS Model Comment"
--- base/test/MainSequenceModelSpec.hs
+++ base/test/MainSequenceModelSpec.hs
@@ -13,7 +13,11 @@ spec = parallel $ do
   describe "MS Model file format" $ do
     describe "Filters" $ do
       it "parses a single filter" $
-        parseOnly parseFilters "%f U\n" == Right ["U"]
+        parseOnly parseFilters "%f U\n" == (Right $ Filters ["U"])
 
       it "parses a list of filters" $
-        parseOnly parseFilters "%f U B V R I J H K\n" == Right ["U", "B", "V", "R", "I", "J", "H", "K"]
+        parseOnly parseFilters "%f U B V R I J H K\n" == (Right $ Filters ["U", "B", "V", "R", "I", "J", "H", "K"])
+
+    describe "Comments" $ do
+      it "captures a comment" $
+        parseOnly parseComment "# any text here\n" == (Right $ Comment " any text here")
