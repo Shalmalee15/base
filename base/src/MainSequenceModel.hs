2591ac8d0ffcb472c390d5f3345f78c230a1261c
Elliot Robinson <elliot.robinson@argiopetech.com>
2019-02-03 14:18:50 -0500

    Limbo-ing! See tags/releases for what you'd normally expect in a commit message.

--- base/src/MainSequenceModel.hs
+++ base/src/MainSequenceModel.hs
@@ -4,3 +4,6 @@ import Data.Text
 import qualified Data.Attoparsec.Text as A
 
 data Model = Model { filters :: [Text] }
+
+parseFilters :: Text -> [Text]
+parseFilters t = ["U"]
--- base/test/MainSequenceModelSpec.hs
+++ base/test/MainSequenceModelSpec.hs
@@ -2,10 +2,14 @@ module MainSequenceModelSpec (main, spec) where
 
 import Test.Hspec
 
+import MainSequenceModel
+
+
 main :: IO ()
 main = hspec spec
 
 spec = parallel $ do
   describe "MS Model file format" $ do
     describe "Filters" $ do
-      it "passes a test" $ True == True
+      it "parses a single filter" $
+        (parseFilters "U") == ["U"]
