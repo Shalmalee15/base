639de6df0eaca1718609e04bd5a7e1c30ef4fb28
Elliot Robinson <elliot.robinson@argiopetech.com>
2019-02-05 15:30:24 -0500

    Limbo-ing! See tags/releases for what you'd normally expect in a commit message.

--- base/test/MainSequenceModelSpec.hs
+++ base/test/MainSequenceModelSpec.hs
@@ -73,30 +73,37 @@ spec = parallel $ do
 
 
     describe "Model" $ do
-      let doParse = runConduitPure $ yield dsed .| lexModel .| filterC (either (const True) (not . isComment . snd)) .| sinkList
+      let doParse = runConduitPure $ yield dsed .| lexModel .| sinkList
 
       it "parses the sections" $
         let result = sequence $ map (fmap snd) doParse
         in (result `shouldSatisfy` isRight) >> ((\(Right r) -> r) result `shouldBe` expected)
-            where expected = [ Filters ["U", "B", "V"]
+            where expected = [ Comment "(abbreviated) DSED models"
+                             , Filters ["U", "B", "V"]
                              , SectionHeader (-2.5) 0 1.938 0.2451
                              , AgeHeader 8.39794
+                             , Comment "EEP     Mass         U         B         V"
                              , EEP 2 0.278163 [11.7478, 11.0484, 9.8499]
                              , EEP 3 0.318852 [11.3514, 10.7092, 9.5412]
                              , EEP 4 0.335466 [11.2028, 10.5813, 9.4241]
                              , EEP 5 0.351598 [11.0572, 10.4578, 9.3119]
                              , AgeHeader 8.477121
+                             , Comment "EEP     Mass         U         B         V"
                              , EEP 2 0.212681 [12.5728, 11.7446, 10.4768]
                              , EEP 3 0.290489 [11.6188, 10.9382,  9.7498]
                              , EEP 4 0.320389 [11.3348, 10.6947,  9.5277]
                              , EEP 5 0.335518 [11.2034, 10.5822,  9.4251]
+                             , Comment ""
+
                              , SectionHeader (-2.0) 0.0 1.938 0.2453
                              , AgeHeader 8.397940
+                             , Comment "EEP     Mass         U         B         V"
                              , EEP 2 0.297801 [12.1589, 11.2562, 9.9655]
                              , EEP 3 0.335484 [11.8031, 10.9432, 9.6821]
                              , EEP 4 0.338823 [11.7674, 10.9126, 9.6546]
                              , EEP 5 0.355097 [11.5974, 10.7646, 9.5203]
                              , AgeHeader 8.477121
+                             , Comment "EEP     Mass         U         B         V"
                              , EEP 2 0.251276 [12.6621, 11.6918, 10.3548]
                              , EEP 3 0.317207 [11.9778, 11.0959,  9.8205]
                              , EEP 4 0.335075 [11.8076, 10.9477,  9.6866]
@@ -119,6 +126,7 @@ dsed = B.pack $ [r|# (abbreviated) DSED models
     3 0.290489 11.618800 10.938200  9.749800
     4 0.320389 11.334800 10.694700  9.527700
     5 0.335518 11.203400 10.582200  9.425100
+
 %s [Fe/H]=-2.000000    [alpha/Fe]=0.000000    l/Hp=1.938000    Y=0.245300
 %a logAge=8.397940
 # EEP     Mass         U         B         V
