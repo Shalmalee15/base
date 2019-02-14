6900ec1ad5b751f88c5ad0e4d7c630b20c3b1680
Elliot Robinson <elliot.robinson@argiopetech.com>
2019-02-13 21:36:43 -0500

    Limbo-ing! See tags/releases for what you'd normally expect in a commit message.

--- base/makeIsochrone/Main.hs
+++ base/makeIsochrone/Main.hs
@@ -9,15 +9,34 @@ import Data.ByteString (ByteString)
 import Data.Conduit.Lzma
 import Data.Either (isRight)
 import qualified Data.Set as S
+import qualified Data.Vector.Unboxed as V
+
+import Graphics.Rendering.Chart.Easy
+import Graphics.Rendering.Chart.Backend.Cairo
 
 import MainSequenceModel
 
 main :: IO ()
 main = do
-  (Just t) <- runConduitRes ( sourceFile "/home/elliot/projects/base-models/dsed/dsed_new.model.xz"
-                           .| decompress Nothing
-                           .| lexModel
-                           .| parseModel
-                           .| lastC )
+  t <- runConduitRes ( sourceFile "../../base-models/dsed/dsed_new.model.xz"
+                     .| decompress Nothing
+                     .| lexModel
+                     .| parseModel
+                     .| sinkList )
 
-  print $ S.map PrettyAge $ snd t
+  let (feh, ages) = t !! 4
+      go (Age a _ _ fs) =
+        let b = fs !! 1
+            v = fs !! 2
+            bmv = V.zipWith (-) b v
+        in plot $ do
+          pts <- points (show a) $ V.toList $ V.zip bmv v
+          return $ pts & plot_points_style . point_radius .~ 1
+
+  toFile (def & (fo_size .~ (1920, 1440)) & (fo_format .~ SVG)) "/tmp/test.svg" $ do
+    setColors (map opaque [blue, green, purple, yellow, red])
+
+    layout_title .= "New DSED, [Fe/H] = " ++ show feh
+    layout_x_axis .= (def & laxis_title .~ "B - V")
+    layout_y_axis .= (def & laxis_reverse .~ True & laxis_title .~ "V")
+    mapM_ go $ S.elems ages
