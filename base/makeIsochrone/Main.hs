8a541875e4d11dccffadbb3375cee71b7b6cc7c1
Elliot Robinson <elliot.robinson@argiopetech.com>
2019-03-29 20:03:21 -0400

    Limbo-ing! See tags/releases for what you'd normally expect in a commit message.

--- base/makeIsochrone/Main.hs
+++ base/makeIsochrone/Main.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE TypeApplications #-}
 module Main where
 
 import Conduit
@@ -14,11 +15,27 @@ import qualified Data.Vector.Unboxed as V
 import Graphics.Rendering.Chart.Easy
 import Graphics.Rendering.Chart.Backend.Cairo
 
+import System.Environment
+
 import MainSequenceModel
+
+data MSModel = OldDSED | NewDSED | Yale2018 | PARSEC
+             deriving (Read, Show)
+
+class HasModelPath a where
+  modelPath :: a -> FilePath -> FilePath
+
+instance HasModelPath MSModel where
+  modelPath OldDSED  base = base ++ "mainSequence/dsed_old.model.xz"
+  modelPath NewDSED  base = base ++ "mainSequence/dsed_new.model.xz"
+  modelPath PARSEC   base = base ++ "mainSequence/PARSEC.model.xz"
+  modelPath Yale2018 base = base ++ "mainSequence/yale_2018.model.xz"
 
 main :: IO ()
 main = do
-  runConduitRes ( sourceFile "../../base-models/dsed/dsed_new.model.xz"
+  cliArgs <- read @MSModel . head <$> getArgs
+
+  runConduitRes ( sourceFile (modelPath cliArgs "models/")
                 .| decompress Nothing
                 .| lexModel
                 .| parseModel
@@ -34,14 +51,14 @@ toFeHPlot t = do
           pts <- points (show a) $ V.toList $ V.zip bmv v
           return $ pts & plot_points_style . point_radius .~ 1
 
-  toFile (def & (fo_size .~ (1920, 1440))
-              & (fo_format .~ SVG)
-         ) ("/tmp/feh" ++ (if feh < 0 then 'm' else 'p'):show (abs feh) ++ "_y" ++ show y ++ ".svg") $ do
+  toFile (def & (fo_size .~ (7680, 5000)) -- (1920, 4000)) --1440))
+              & (fo_format .~ PNG)
+         ) ("/tmp/feh" ++ (if feh < 0 then 'm' else 'p'):show (abs feh) ++ "_y" ++ show y ++ ".png") $ do
     setColors (map opaque [blue, green, purple, yellow, red])
 
     layout_title .= "New DSED, [Fe/H] = " ++ show feh ++ ", Y = " ++ show y
-    layout_x_axis .= (def & laxis_title .~ "B - V"
-                          & laxis_generate .~ scaledAxis def (-0.5, 2))
+    layout_x_axis .= (def & laxis_title .~ "B - V")
+--                          & laxis_generate .~ scaledAxis def (-0.5, 2))
     layout_y_axis .= (def & laxis_reverse .~ True & laxis_title .~ "V"
-                          & laxis_generate .~ scaledAxis def (-5, 15))
+                          & laxis_generate .~ scaledAxis def (-8, 15))
     mapM_ go $ S.elems ages
