e5d0c4d5c5e28ed0e766df12f74d578cb0e0ac4a
Elliot Robinson <elliot.robinson@argiopetech.com>
2019-04-01 17:54:54 -0400

    Limbo-ing! See tags/releases for what you'd normally expect in a commit message.

--- base/makeIsochrone/Main.hs
+++ base/makeIsochrone/Main.hs
@@ -1,12 +1,22 @@
 module Main where
 
+def :: [a]
 def = []
 
 
 main :: IO ()
 main = do options <- loadOptions def
+          models <- loadModels (model options)
           undefined
 
 
 loadOptions :: Monoid a => a -> b
-loadOptions a = undefined
+loadOptions _ = undefined
+
+
+loadModels :: String -> a
+loadModels _ = undefined
+
+
+model :: a -> b
+model _ = undefined
