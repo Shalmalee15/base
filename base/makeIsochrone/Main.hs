54aa5ba2f4f2e5cde1cc37f645992fe8e671b6bd
Elliot Robinson <elliot.robinson@argiopetech.com>
2019-04-03 20:36:38 -0400

    Limbo-ing! See tags/releases for what you'd normally expect in a commit message.

--- base/makeIsochrone/Main.hs
+++ base/makeIsochrone/Main.hs
@@ -3,6 +3,7 @@ module Main where
 import Options
 import Output.Sql
 import Models.Input
+import Models.Interpolate
 
 import System.Console.CmdArgs.Implicit
 
@@ -16,9 +17,3 @@ main = do options <- loadOptions (clusterOptions)
           models <- loadModels (model options)
           let interpolated = interpolateIsochrone Nearest models (cluster options)
           withDB "db.base" $ \db -> storePhotometry interpolated
-
-
-data InterpolationMethod = Nearest
-
-interpolateIsochrone :: InterpolationMethod -> p2 -> p3 -> a
-interpolateIsochrone _ _ _ = undefined
--- base/src/Models/Interpolate.hs
+++ base/src/Models/Interpolate.hs
@@ -1,1 +1,7 @@
 module Models.Interpolate where
+
+data InterpolationMethod = Nearest
+
+
+interpolateIsochrone :: InterpolationMethod -> p2 -> p3 -> a
+interpolateIsochrone _ _ _ = undefined
