306c509f8fe8da5368b173b9d1f4438290b22532
Elliot Robinson <elliot.robinson@argiopetech.com>
2019-04-06 18:17:44 -0400

    Limbo-ing! See tags/releases for what you'd normally expect in a commit message.

--- base/test/OptionsSpec.hs
+++ base/test/OptionsSpec.hs
@@ -1,14 +1,6 @@
 {-# LANGUAGE QuasiQuotes, OverloadedLists #-}
 module OptionsSpec (main, spec) where
 
-import Conduit
-
-import Data.Attoparsec.ByteString (parseOnly)
-import Data.Either (isLeft, isRight)
-
-import           Data.ByteString (ByteString)
-import qualified Data.ByteString.Char8 as B
-
 import Test.Hspec
 
 
