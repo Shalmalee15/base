2917c290ea74031e80995f25df90446b964d029f
Elliot Robinson <elliot.robinson@erau.edu>
2019-04-06 20:57:04 -0400

    Limbo-ing! See tags/releases for what you'd normally expect in a commit message.

--- base/makeIsochrone/Main.hs
+++ base/makeIsochrone/Main.hs
@@ -1,18 +1,7 @@
 module Main where
 
-import Options
-import Output.Sql
-import Models.Input
-import Models.Interpolate
-
 import Options.Applicative
-
-clusterOptions :: Parser a
-clusterOptions = undefined
 
 
 main :: IO ()
-main = do options <- loadOptions clusterOptions
-          models <- loadModels (model options)
-          let interpolated = interpolateIsochrone Nearest models (cluster options)
-          withDB "db.base" $ \db -> storePhotometry interpolated
+main = undefined
