6345f9fa04c7a3fbf136c170cf704e475ac11323
Elliot Robinson <elliot.robinson@argiopetech.com>
2019-04-15 21:10:15 -0400

    Limbo-ing! See tags/releases for what you'd normally expect in a commit message.

--- base/makeIsochrone/Main.hs
+++ base/makeIsochrone/Main.hs
@@ -44,7 +44,7 @@ main :: IO ()
 main = do options <- execParser opts
           models  <- loadModels OldDSED
           if length models /= 0 then (print $ S.map PrettyAge $ head $ map snd models) else return ()
-          if length models /= 0 then (print $ head $ map fst models) else return ()
+          if length models /= 0 then (print $ map fst models) else return ()
           print $ interpolateIsochrone (cluster options & feh, cluster options & y, cluster options & age) models
   where
     opts = info (makeIsochroneOptionParser <**> helper)
