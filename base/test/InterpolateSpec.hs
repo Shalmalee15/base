4653c7c50deb71233147a52235188e87e1d8b06b
Elliot Robinson <elliot.robinson@argiopetech.com>
2019-04-15 21:17:56 -0400

    Limbo-ing! See tags/releases for what you'd normally expect in a commit message.

--- base/test/InterpolateSpec.hs
+++ base/test/InterpolateSpec.hs
@@ -1,10 +1,12 @@
 module InterpolateSpec (main, spec) where
 
 import Test.Hspec
-
+import Interpolate
 
 main :: IO ()
 main = hspec spec
 
 spec :: SpecWith ()
-spec = pure ()
+spec = do describe "interpolation" $ do
+            it "should function"
+               (interpolateIsochrone (0, 0, 0) [] `shouldBe` [0, 0, 0])
