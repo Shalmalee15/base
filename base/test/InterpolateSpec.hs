06d555e5e31649263f8bcc7d2c50824b165d4d2e
Elliot Robinson <elliot.robinson@argiopetech.com>
2019-04-22 08:56:12 -0400

    Limbo-ing! See tags/releases for what you'd normally expect in a commit message.

--- base/test/InterpolateSpec.hs
+++ base/test/InterpolateSpec.hs
@@ -12,13 +12,17 @@ spec = do
     it "should function"
        (interpolateIsochrone (0, 0, 0) [] `shouldBe` [0, 0, 0])
   describe "linear interpolation" $ do
-    it "returns x1 when f = 0 and delta = 0"
-       (linearInterpolate 0.0 0.0 0.0 `shouldBe` 0.0)
-    it "returns x2 when f = 1 and delta = 0"
-       (linearInterpolate 0.0 0.0 1.0 `shouldBe` 0.0)
-    it "returns x1 when f = 0"
-       (linearInterpolate 0.0 5.0 0.0 `shouldBe` 0.0)
-    it "returns x2 when f = 1"
-       (linearInterpolate 0.0 5.0 1.0 `shouldBe` 5.0)
-    it "returns halfway between x1 and x2 when f = 0.5"
-       (linearInterpolate 0.0 5.0 0.5 `shouldBe` 2.5)
+    describe "when delta x = 0" $ do
+      it "returns x1 when f = 0"
+         (linearInterpolate 0.0 0.0 0.0 `shouldBe` 0.0)
+      it "returns x2 when f = 1"
+         (linearInterpolate 0.0 0.0 1.0 `shouldBe` 0.0)
+      it "returns x1 or x2"
+         (linearInterpolate 0.0 0.0 0.5 `shouldBe` 0)
+    describe "when delta x = 5" $ do
+      it "returns x1 when f = 0"
+         (linearInterpolate 0.0 5.0 0.0 `shouldBe` 0.0)
+      it "returns x2 when f = 1"
+         (linearInterpolate 0.0 5.0 1.0 `shouldBe` 5.0)
+      it "returns halfway between x1 and x2 when f = 0.5"
+         (linearInterpolate 0.0 5.0 0.5 `shouldBe` 2.5)
