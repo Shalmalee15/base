35594ded50ced94d1ab71f4dfd6bf78a869cc055
Elliot Robinson <elliot.robinson@argiopetech.com>
2019-04-22 09:39:52 -0400

    Limbo-ing! See tags/releases for what you'd normally expect in a commit message.

--- base/test/InterpolateSpec.hs
+++ base/test/InterpolateSpec.hs
@@ -8,9 +8,29 @@ main = hspec spec
 
 spec :: SpecWith ()
 spec = do
+  linearInterpolateSpec
+
   describe "isochrone interpolation" $ do
     it "should function"
        (interpolateIsochrone (0, 0, 0) [] `shouldBe` [0, 0, 0])
+
+  describe "log interpolation (per paper)" $ do
+    it "returns x1 when f = 0"
+       (logInterpolate (log 1) (log 5) 0.0 `shouldBe` (log 1))
+    it "returns x2 when f = 1"
+       (logInterpolate (log 1) (log 5) 1.0 `shouldBe` (log 5))
+    xit "returns x1 or x2"
+        (logInterpolate (log 1) (log 5) 0.5 `shouldBe` (log 1))
+    it "returns x1 when f = 0"
+       (logInterpolate (log 2) (log 3) 0.0 `shouldBe` (log 2))
+    it "returns x2 when f = 1"
+       (logInterpolate (log 2) (log 3) 1.0 `shouldBe` (log 3))
+    xit "returns x1 or x2"
+        (logInterpolate (log 2) (log 3) 0.5 `shouldBe` (log 2))
+
+
+linearInterpolateSpec :: SpecWith ()
+linearInterpolateSpec =
   describe "linear interpolation" $ do
     describe "when delta x = 0" $ do
       it "returns x1 when f = 0"
@@ -19,6 +39,7 @@ spec = do
          (linearInterpolate 0.0 0.0 1.0 `shouldBe` 0.0)
       it "returns x1 or x2"
          (linearInterpolate 0.0 0.0 0.5 `shouldBe` 0.0)
+
     describe "when delta x = 5" $ do
       it "returns x1 when f = 0"
          (linearInterpolate 0.0 5.0 0.0 `shouldBe` 0.0)
@@ -26,6 +47,7 @@ spec = do
          (linearInterpolate 0.0 5.0 1.0 `shouldBe` 5.0)
       it "returns halfway between x1 and x2 when f = 0.5"
          (linearInterpolate 0.0 5.0 0.5 `shouldBe` 2.5)
+
     describe "when delta x = 4 and x1 is 1" $ do
       it "returns x1 when f = 0"
          (linearInterpolate 1.0 5.0 0.0 `shouldBe` 1.0)
@@ -33,16 +55,3 @@ spec = do
          (linearInterpolate 1.0 5.0 1.0 `shouldBe` 5.0)
       it "returns halfway between x1 and x2 when f = 0.5"
          (linearInterpolate 1.0 5.0 0.5 `shouldBe` 3.0)
-    describe "log interpolation (per paper)" $ do
-      it "returns x1 when f = 0"
-         (logInterpolate (log 1) (log 5) 0.0 `shouldBe` (log 1))
-      it "returns x2 when f = 1"
-         (logInterpolate (log 1) (log 5) 1.0 `shouldBe` (log 5))
-      xit "returns x1 or x2"
-          (logInterpolate (log 1) (log 5) 0.5 `shouldBe` (log 1))
-      it "returns x1 when f = 0"
-         (logInterpolate (log 2) (log 3) 0.0 `shouldBe` (log 2))
-      it "returns x2 when f = 1"
-         (logInterpolate (log 2) (log 3) 1.0 `shouldBe` (log 3))
-      xit "returns x1 or x2"
-          (logInterpolate (log 2) (log 3) 0.5 `shouldBe` (log 2))
