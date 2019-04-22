2550b0d4cda48e94a158b73ee369787b612e3f2d
Elliot Robinson <elliot.robinson@argiopetech.com>
2019-04-22 09:38:11 -0400

    Limbo-ing! See tags/releases for what you'd normally expect in a commit message.

--- base/test/InterpolateSpec.hs
+++ base/test/InterpolateSpec.hs
@@ -26,6 +26,13 @@ spec = do
          (linearInterpolate 0.0 5.0 1.0 `shouldBe` 5.0)
       it "returns halfway between x1 and x2 when f = 0.5"
          (linearInterpolate 0.0 5.0 0.5 `shouldBe` 2.5)
+    describe "when delta x = 4 and x1 is 1" $ do
+      it "returns x1 when f = 0"
+         (linearInterpolate 1.0 5.0 0.0 `shouldBe` 1.0)
+      it "returns x2 when f = 1"
+         (linearInterpolate 1.0 5.0 1.0 `shouldBe` 5.0)
+      it "returns halfway between x1 and x2 when f = 0.5"
+         (linearInterpolate 1.0 5.0 0.5 `shouldBe` 3.0)
     describe "log interpolation (per paper)" $ do
       it "returns x1 when f = 0"
          (logInterpolate (log 1) (log 5) 0.0 `shouldBe` (log 1))
