eddb35c5c535cc15ddb5ebcc00f5f85bc67dffc6
Elliot Robinson <elliot.robinson@argiopetech.com>
2019-04-22 08:48:08 -0400

    Limbo-ing! See tags/releases for what you'd normally expect in a commit message.

--- base/test/InterpolateSpec.hs
+++ base/test/InterpolateSpec.hs
@@ -11,5 +11,12 @@ spec = do
   describe "isochrone interpolation" $ do
     it "should function"
        (interpolateIsochrone (0, 0, 0) [] `shouldBe` [0, 0, 0])
-  describe "linear interpolation" $ do it "should linearly interpolate"
-                                          (linearInterpolate 0.0 0.0 0.0 `shouldBe` 0.0)
+  describe "linear interpolation" $ do
+    it "returns x1 when f = 0 and delta = 0"
+       (linearInterpolate 0.0 0.0 0.0 `shouldBe` 0.0)
+    it "returns x2 when f = 1 and delta = 0"
+       (linearInterpolate 0.0 0.0 1.0 `shouldBe` 0.0)
+    it "returns x1 when f = 0"
+       (linearInterpolate 0.0 5.0 0.0 `shouldBe` 0.0)
+    it "returns x2 when f = 1"
+       (linearInterpolate 0.0 5.0 1.0 `shouldBe` 5.0)
