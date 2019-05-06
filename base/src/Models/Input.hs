b7e5b5be8ca19fb87ee9954013d4fc44dfbfaad1
Elliot Robinson <elliot.robinson@argiopetech.com>
2019-05-06 05:45:56 -0400

    Limbo-ing! See tags/releases for what you'd normally expect in a commit message.

--- base/src/Models/Input.hs
+++ base/src/Models/Input.hs
@@ -1,4 +1,4 @@
-module Models.Input where
+module Models.Input (loadModels) where
 
 import Conduit
 
@@ -7,8 +7,20 @@ import qualified Data.Set as S
 
 import MainSequenceModel
 import Paths
-
+import Types
 
 loadModels :: (MonadThrow m, HasModelPath p, MonadUnliftIO m) => p -> m [((Double, Double), S.Set Age)]
 loadModels model = runConduitRes $ loadModel .| sinkList
   where loadModel = sourceFile (modelPath model "models/") .| decompress Nothing .| lexModel .| parseModel
+
+
+-- type EEP = Positive
+
+-- data CAge = CAge LogAge (V.Vector EEP) (V.Vector Mass) [V.Vector Magnitude] deriving (Eq, Show)
+
+convertModels :: [((Double, Double), S.Set Age)] -> [((FeH, HeliumFraction), S.Set Age)]
+convertModels = map go
+  where go ((feh, y), isochrone) =
+          let feh' = MkFeH . packLog $ feh
+              y'   = MkHeliumFraction . MkPercentage . closedUnitInterval' $ y
+          in ((feh', y'), isochrone)
