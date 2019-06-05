c46235fd3d1328620585f75d58323783d1a5a24c
Elliot Robinson <elliot.robinson@argiopetech.com>
2019-06-04 20:02:24 -0400

    Limbo-ing! See tags/releases for what you'd normally expect in a commit message.

--- models/testModelArchive/Main.hs
+++ models/testModelArchive/Main.hs
@@ -1,4 +1,32 @@
 module Main where
 
+import Conduit
+
+import Control.Monad (void)
+import Data.Conduit.Lzma
+import Data.Set  (Set)
+import Data.Text (Text)
+
+import Options.Applicative
+
+import Paths
+import MainSequenceModel
+
+type RawModel = [(([Text], Double, Double), Set Age)]
+
+loadModels :: (HasModelPath p) => p -> IO RawModel
+loadModels model = runConduitRes $ loadModel .| sinkList
+  where loadModel = sourceFile (modelPath model "") .| decompress Nothing .| lexModel .| parseModel
+
+newtype ModelArchive = MkModelArchive { unModelArchive :: String }
+
+instance HasModelPath ModelArchive where
+  modelPath a _ = unModelArchive a
+
 main :: IO ()
-main = undefined
+main = do options <- execParser opts
+          void $ loadModels options
+  where
+    opts = info (option (maybeReader (Just . MkModelArchive)) (long "modelFile" <> help "Specify model archive") <**> helper)
+      ( fullDesc
+     <> progDesc "Generate an isochrone from the models based on cluster parameters")
