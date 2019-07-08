884831b7348d325a852085320fa648cb28e0a957
Elliot Robinson <elliot.robinson@argiopetech.com>
2019-07-08 06:10:19 -0400

    Limbo-ing! See tags/releases for what you'd normally expect in a commit message.

--- base/src/Magnitude.hs
+++ base/src/Magnitude.hs
@@ -1,1 +1,14 @@
-module Magnitude where
+{-# LANGUAGE TemplateHaskell, TypeFamilies, MultiParamTypeClasses #-}
+module Magnitude (AbsoluteMagnitude(..)) where
+
+import Types.Internal
+import Data.Vector.Unboxed (Vector)
+import Data.Vector.Unboxed.Deriving
+
+newtype AbsoluteMagnitude = MkAbsoluteMagnitude { unAbsoluteMagnitude :: Log10 }
+        deriving (Show, Eq, Ord)
+
+derivingUnbox "AbsoluteMagnitude"
+  [t| AbsoluteMagnitude -> Log10 |]
+  [| unAbsoluteMagnitude |]
+  [| MkAbsoluteMagnitude |]
