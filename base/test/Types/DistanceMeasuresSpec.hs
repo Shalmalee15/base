99dd3340c293192d65d9192c2a3bbb0a34cc674d
Elliot Robinson <elliot.robinson@argiopetech.com>
2019-07-09 06:37:43 -0400

    Limbo-ing! See tags/releases for what you'd normally expect in a commit message.

---
+++ base/test/Types/DistanceMeasuresSpec.hs
@@ -0,0 +1,11 @@
+module Types.DistanceMeasuresSpec (main, spec) where
+
+import Test.Hspec
+
+import Types.DistanceMeasures
+
+main :: IO ()
+main = hspec spec
+
+spec :: SpecWith ()
+spec = pure ()
